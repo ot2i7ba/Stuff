ad345364f1a3ac10d3c086be84d2e871ef9a50e0
ot2i7ba <86646393+ot2i7ba@users.noreply.github.com>
2023-02-27 10:44:21 +0100

    Create plotlyimex.py

---
+++ plotlyimex/plotlyimex.py
@@ -0,0 +1,45 @@
+# By ot2i7ba 2023-02-27
+
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
+import os
+
+# Ask user for CSV file name and add '.csv' file extension if missing
+csv_file = input("Input csv filename (enter for 'import.csv'): ")
+if csv_file == '':
+    csv_file = 'import.csv'
+else:
+    if not csv_file.endswith('.csv'):
+        csv_file += '.csv'
+
+# Check if the file is a CSV file
+if not os.path.isfile(csv_file):
+    print("Error: The file '" + csv_file + "' could not be found.")
+elif not os.path.splitext(csv_file)[1] == '.csv':
+    print("Error: The file '" + csv_file + "' is not a CSV file.")
+else:
+    # Import CSV file
+    df = pd.read_csv(csv_file)
+
+    # Create map using OpenStreetMap
+    fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name="id",
+                            hover_data=["userId", "lastSeenAt", "speed", "direction", "source"],
+                            zoom=3)
+
+    #fig.update_layout(mapbox_style="open-street-map", height=1080, width=1920)
+    fig.update_layout(mapbox_style="carto-darkmatter", height=1080, width=1920)
+
+    # Scale the height and width to 90%
+    # fig.update_layout(height=0.9*fig.layout.height, width=0.9*fig.layout.width)
+
+    # Automatically set the name of the exported file to the name of the imported file, if the user has entered a name
+    html_file = input("Output html filename (enter for input filename): ")
+    if html_file == '':
+        html_file = os.path.splitext(csv_file)[0] + '.html'
+    elif not html_file.endswith('.html'):
+        html_file += '.html'
+
+    # Display or save plot
+    pio.show(fig)
+    pio.write_html(fig, file=html_file)
